435fcfcce3c3afd6a3499414baf1e22a48b44f87
mostafijurmrs <mostafijurrahmanmrs241@gmail.comm>
2022-11-08 17:37:58 +0600

    added the firebase authintication and provied the google and email login method

---
+++ src/Context/AuthProvider.js
@@ -0,0 +1,72 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
+import app from '../firebase/firebase.config';
+
+
+export const AuthContext = createContext();
+const auth = getAuth(app)
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // google log in method
+    const loginProvider = (provider) => {
+        setLoading(true);
+        return signInWithPopup(auth, provider)
+    }
+
+    // email register sign up  method 
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // sign in method
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    // user update
+    const updateUserProfile = (profile) => {
+        return updateProfile(auth.currentUser, profile)
+    }
+
+      // log out 
+      const logOut = () => {
+        setLoading(true);
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log('user inside state changed', currentUser);
+            setLoading(false);
+            setUser(currentUser);
+        })
+
+        return () => {
+            unSubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loginProvider,
+        logOut,
+        createUser,
+        signIn,
+        loading,
+        updateUserProfile,
+    
+    }
+    
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/Pages/Login&Register/Register/Register.js
+++ src/Pages/Login&Register/Register/Register.js
@@ -1,16 +1,70 @@
-import React from 'react';
+import { GoogleAuthProvider } from 'firebase/auth';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../Context/AuthProvider';
+import { FaGoogle } from "react-icons/fa";
 
 const Register = () => {
+
+    const [error, setError] = useState('');
+    const { createUser, updateUserProfile, loginProvider } = useContext(AuthContext);
+
+    //  google sign in method
+    const googleProvider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        loginProvider(googleProvider)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.error(error))
+    }
+
+
+    // email registration  sign in method
+    const handleSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, password);
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset();
+                setError('');
+                handleUpdateUserProfile(name);
+                // navigate('/')
+            })
+            .catch(error => {
+                console.error(error);
+                setError(error.message);
+            })
+    }
+
+    const handleUpdateUserProfile = (name) => {
+        const profile = {
+            displayName: name,
+        }
+        updateUserProfile(profile)
+            .then(() => { })
+            .catch(error => console.error(error))
+    }
+
+
     return (
         <div className="hero w-full my-20">
             <div className="hero-content grid gap-6 md:grid-cols-2 flex-col lg:flex-row">
                 <div className="text-center lg:text-left">
                     <img className='w-full' src='https://i.ibb.co/MVVm1Hx/password.webp' alt="" />
                 </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 my-6">
                     <h1 className="text-5xl text-center font-bold">Sign Up</h1>
-                    <form className="card-body">
+                    <form onSubmit={handleSubmit} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
@@ -30,11 +84,28 @@ const Register = () => {
                             <input type="password" name='password' placeholder="password" className="input input-bordered" required />
 
                         </div>
+                        <p className='text-2xl text-red-600'>
+                            {error}
+                        </p>
                         <div className="form-control mt-6">
                             <input className="btn btn-primary" type="submit" value="Sign Up" />
                         </div>
                     </form>
                     <p className='text-center'>Already have an account? <Link className='text-orange-600 font-bold' to="/login">Login</Link> </p>
+
+                    <div className="flex items-center pt-4 space-x-1">
+                        <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+                        <p className="px-3 text-sm dark:text-gray-400">Login with social accounts</p>
+                        <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+                    </div>
+                    <div className="flex justify-center space-x-4 mb-12 mt-6">
+                        <button
+                            onClick={handleGoogleSignIn}
+                            className="p-3 btn btn-info flex items-center justify-center">
+                            <FaGoogle className='text-2xl text-red-700'></FaGoogle>
+                            <span className='text-xl text-bold ml-2'>Google Sign In</span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
