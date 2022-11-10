48879977c6db88cbf79c8e4d6ff1a07cb8e23fe8
mostafijurmrs <mostafijurrahmanmrs241@gmail.comm>
2022-11-10 21:37:23 +0600

    added the service detail page for the each service review

--- src/Pages/Services/AddRevieewDisplay.js
+++ src/Pages/Services/AddRevieewDisplay.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
-const AddRevieewDisplay = ({}) => {
-    
+const AddRevieewDisplay = ({ rvw }) => {
+    const { reviewMessage, customer, photoUser } = rvw;
     return (
         <div>
-            
+            <div className=" flex flex-col px-24 my-5 w-full p-6 mx-auto divide-y rounded-md divide-gray-700 bg-gray-900 text-gray-100">
+                <div className="flex justify-between p-4">
+                    <div className="flex space-x-4">
+                        <div>
+                            <img src={photoUser} alt="" className="object-cover w-12 h-12 rounded-full bg-gray-500" />
+                        </div>
+                        <div>
+                            <h4 className="font-bold">{customer}</h4>
+                            <span className="text-xs text-gray-400">{ }</span>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="p-4 space-y-2 text-sm text-gray-400">
+                    <p>{reviewMessage}</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Services/DetailServices.js
+++ src/Pages/Services/DetailServices.js
@@ -1,9 +1,10 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import 'react-photo-view/dist/react-photo-view.css';
 import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { Link, useLoaderData } from 'react-router-dom';
 import { FaStar, FaStarHalf } from "react-icons/fa";
 import { AuthContext } from '../../Context/AuthProvider';
+import AddRevieewDisplay from './AddRevieewDisplay';
 
 const DetailServices = () => {
     const [review, setReview] = useState({});
@@ -27,13 +28,13 @@ const DetailServices = () => {
         }
         setReview(reviewDetails);
         // form.reset("");
-       
+
     }
 
     const handleAddToReview = e => {
         e.preventDefault();
         const form = e.target;
-        
+
 
         fetch('https://assignment-11-service-server-side.vercel.app/reviews', {
             method: 'POST',
@@ -55,6 +56,11 @@ const DetailServices = () => {
             .catch(err => console.error(err));
 
     }
+    useEffect(() => {
+        fetch(`https://assignment-11-service-server-side.vercel.app/reviews/${_id}`)
+            .then(res => res.json())
+            .then(data => setAddedReview(data))
+    }, [_id])
 
 
     return (
@@ -97,54 +103,57 @@ const DetailServices = () => {
                 </div>
             </div>
 
-            <div className='flex  justify-center shadow-2xl'>
-                <div>
-                    {
-                        user?.email ?
-                            <div className="flex flex-col w-full p-8 shadow-sm rounded-xl lg:p-12 bg-emerald-700 text-gray-100">
-                                <div className="flex flex-col  w-full">
-                                    <h2 className="text-3xl">User Name : {user.displayName}</h2>
-                                    <h4 className="text-xl mb-4">Photography item is {title}</h4>
-                                    <h2 className="text-3xl font-semibold text-center">Your Review and feedback mettars!</h2>
-                                    <div className="flex flex-col items-center py-6 space-y-2">
-                                        <span className="text-center">How was your experience?</span>
+            <div>
+                <div className='flex  justify-center shadow-2xl'>
+                    <div>
+                        {
+                            user?.email ?
+                                <div className="flex flex-col w-full p-8 shadow-sm rounded-xl lg:p-12 bg-emerald-700 text-gray-100">
+                                    <div className="flex flex-col  w-full">
+                                        <h2 className="text-3xl">User Name : {user.displayName}</h2>
+                                        <h4 className="text-xl mb-4">Photography item is {title}</h4>
+                                        <h2 className="text-3xl font-semibold text-center">Your Review and feedback mettars!</h2>
+                                        <div className="flex flex-col items-center py-6 space-y-2">
+                                            <span className="text-center">How was your experience?</span>
+
+                                        </div>
+
+                                        {/* review form  */}
+
+                                        <form onSubmit={handleAddToReview} className="flex flex-col w-full">
+                                            <input onBlur={handleInputBlur} rows="3" type="text" placeholder='Write a review...' name='message' className="p-4 rounded-md resize-none text-cyan-900 bg-white" />
+
+                                            <button type="submit" className='text-white text-2xl bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg  mt-4 px-5 py-2.5 text-center mr-2 mb-2'>Add Your Review</button>
+
+                                        </form>
 
                                     </div>
 
-                                    {/* review form  */}
-
-                                    <form onSubmit={handleAddToReview} className="flex flex-col w-full">
-                                        <input onBlur={handleInputBlur} rows="3" type="text" placeholder='Write a review...' name='message' className="p-4 rounded-md resize-none text-cyan-900 bg-white" />
-
-                                        <button type="submit" className='text-white text-2xl bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg  mt-4 px-5 py-2.5 text-center mr-2 mb-2'>Add Your Review</button>
-
-                                    </form>
-
                                 </div>
 
-                            </div>
+                                :
+                                <div className='bg-cyan-800 p-6 w-full rounded-2xl shadow-2xl mt-12'>
 
-                            :
-                            <div className='bg-cyan-800 p-6 w-full rounded-2xl shadow-2xl mt-12'>
-
-                                <p>Please login to give a review!</p>
-                                <button
-                                    className="mt-8 inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
-                                >
-                                    <Link
-                                        to={'/login'}
+                                    <p>Please login to give a review!</p>
+                                    <button
+                                        className="mt-8 inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
                                     >
-                                        Login
-                                    </Link>
-                                </button>
-                            </div>
-                    }
-                </div>
+                                        <Link
+                                            to={'/login'}
+                                        >
+                                            Login
+                                        </Link>
+                                    </button>
+                                </div>
+                        }
+                    </div>
 
+
+                </div>
                 <div>
-                    {/* {
-                addedReview.map(rvw => <Opinions key={rvw._id} rvw={rvw}></Opinions>)
-            } */}
+                    {
+                        addedReview.map(rvw => <AddRevieewDisplay key={rvw._id} rvw={rvw}></AddRevieewDisplay>)
+                    }
                 </div>
             </div>
 
